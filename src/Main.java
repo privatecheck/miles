daf842ed4f6815b8c515446182027f80cd991eca
privatecheck <103067046+privatecheck@users.noreply.github.com>
2022-05-04 00:02:47 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 100; // стоимость билета
+        int oneMile = 20; // рублей нужно для одной мили
+        int bonus = ticketCost / oneMile;
+
+
+        System.out.println("Количество бонусных милль " + bonus);
+    }
 }
